0d14e19c266506c095ec98a186b3378461d05f2f
bitWolfy <bondarchuk.kirill@gmail.com>
2020-09-05 13:12:34 -0700

    Rewrite the login screen message

--- public/auth/login.html.php
+++ public/auth/login.html.php
@@ -11,8 +11,22 @@
     </form>
     <div id="userauth-status"></div>
     <div id="userauth-text">
-        The usage of TagMe! utility requires proper authorization in order to interact with e621's systems.<br />
-        All changes made to the posts are applied with <b>your username</b> and are thus your responsibility.
+        <p>
+            The usage of TagMe! utility requires authorization in order to interact with e621's systems.
+            The API key can be accessed from the <a href="https://e621.net/users/home">account page</a>.
+        </p>
+        <p>
+            All tag changes are made with <b>your username</b>.
+            By using TagMe, you accept full responsibility for any post edits that you make, as well as the consequences of tag abuse.
+        </p>
+        <p>
+            TagMe usese cookies to store your API key. This information is not accessible to the site's administrators in any way.
+            If you believe that your account has been compromised, regenerate the API key immediately and contact e621's administrators.
+        </p>
+        <p>
+            E621 <a href="https://e621.net/wiki_pages/1638">rules</a> and <a href="https://e621.net/static/terms_of_service">terms of service</a> fully apply.
+            Misuse of this utility may lead to your suspension from the service.
+        </p>
     </div>
 </section>
 
