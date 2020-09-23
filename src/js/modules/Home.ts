d67b57ba5f786974c0f0cda39ea899d3d56d4252
bitWolfy <bondarchuk.kirill@gmail.com>
2020-09-22 21:11:56 -0700

    Less jarring change counter on the home page

--- src/js/modules/Home.ts
+++ src/js/modules/Home.ts
@@ -4,7 +4,10 @@ export class Home {
     public static build(): void {
 
         for (const elem of $(".counter").get()) {
-            const demo = new window["countUp"].CountUp(elem, $(elem).attr("count"));
+            const changeCount = parseInt($(elem).attr("count") || "1");
+            const demo = new window["countUp"].CountUp(elem, $(elem).attr("count"), {
+                startVal: Math.round(changeCount * 0.5),
+            });
             if (!demo.error) {
                 demo.start();
             } else {
